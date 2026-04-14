63f236cc331cd1543e34d7504110c37985c8e58d
Aydoucorp <aydoucorp@gmail.com>
2026-04-14 22:47:17 +0200

    Update magazine references in config.js

--- config.js
+++ config.js
@@ -62,7 +62,7 @@ window.FUNNEL_CONFIG = {
     h1: '\u00AB\u00A0J\u2019ai remplac\u00E9 4 cr\u00E8mes anti-\u00E2ge par UN seul stick franco-cor\u00E9en : voici ce qui s\u2019est pass\u00E9 en 14 jours\u00A0\u00BB',
     dek: 'S\u00E9cheresse, ridules, teint terne, rel\u00E2chement, cernes : les 5 signes qui me trahissaient sur 5 zones diff\u00E9rentes du visage. Une amie dermatologue m\u2019a gliss\u00E9 le nom d\u2019un soin cor\u00E9en formul\u00E9 avec un actif brevet\u00E9 fran\u00E7ais. Voici mon journal de 14 jours.',
     productName: 'SAY : PDRN Pink Collagen Volume Multi Balm',
-    cta1Title: 'L\u2019offre que SAY fait aux lectrices de Beaut\u00E9 Mag',
+    cta1Title: 'L\u2019offre que SAY fait aux lectrices d\u2019Eclat Magazine',
     cta1Sub: 'Pack 2 sticks (1 maison + 1 sac) \u00E0 44\u20AC au lieu de 56\u20AC \u00B7 livraison 48h offerte \u00B7 garantie 30 jours',
     cta1Button: 'Voir l\u2019offre SAY \u2192',
     cta1Guarantee: '\u2713 Satisfait ou rembours\u00E9 sans question \u00B7 \u2713 Le seul stick PDRN + Volufiline 5% du march\u00E9 FR',
@@ -77,6 +77,6 @@ window.FUNNEL_CONFIG = {
     testimonial2Text: '\u00AB\u00A0J\u2019avais 4 produits dans ma salle de bain : Caudalie, Nuxe, Dior, et une cr\u00E8me jour. Aujourd\u2019hui je n\u2019ai plus qu\u2019UN stick. Mes sillons sont nettement adoucis et le cou est plus ferme. \u00C9conomie : ~100\u20AC par mois.\u00A0\u00BB',
     testimonial3Name: 'Julie M., 35 ans, Bordeaux',
     testimonial3Text: '\u00AB\u00A0Quand j\u2019ai vu "Volufiline 5% Sederma France" sur le packaging, j\u2019ai compris pourquoi \u00E7a marche. C\u2019est pas du marketing K-beauty, c\u2019est de la vraie science europ\u00E9enne. Mes pattes d\u2019oie ont nettement diminu\u00E9 en 3 semaines.\u00A0\u00BB',
-    footerDisclaimer: 'Beaut\u00E9 Mag est un magazine ind\u00E9pendant. Cet article est sponsoris\u00E9 par SAY France et a \u00E9t\u00E9 r\u00E9dig\u00E9 en partenariat \u00E9ditorial.'
+    footerDisclaimer: 'Eclat Magazine est un magazine ind\u00E9pendant. Cet article est sponsoris\u00E9 par SAY France et a \u00E9t\u00E9 r\u00E9dig\u00E9 en partenariat \u00E9ditorial.'
   }
 };
